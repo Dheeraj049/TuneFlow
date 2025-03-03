c571b4da1548a0ee55fdc773acfc57a64113b29f
Dheeraj <tuneflow8@gmail.com>
2025-03-03 12:00:00 +0530

    Implement Splash Screen Animation

--- app/src/main/java/uk/ac/tees/mad/tuneflow/MainActivity.kt
+++ app/src/main/java/uk/ac/tees/mad/tuneflow/MainActivity.kt
@@ -1,6 +1,9 @@
 package uk.ac.tees.mad.tuneflow
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -47,6 +50,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -59,6 +64,29 @@ import uk.ac.tees.mad.tuneflow.ui.theme.TuneFlowTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+//            setKeepOnScreenCondition {
+//                // Condition to check if the app is still loading and showing the splash screen or not.
+//                TODO()
+//            }
+            setOnExitAnimationListener { screen ->
+                val zoomX = ObjectAnimator.ofFloat(
+                    screen.iconView, View.SCALE_X, 0.4f, 0.0f
+                )
+                zoomX.interpolator = OvershootInterpolator()
+                zoomX.duration = 500L
+                zoomX.doOnEnd { screen.remove() }
+                val zoomY = ObjectAnimator.ofFloat(
+                    screen.iconView, View.SCALE_Y, 0.4f, 0.0f
+                )
+                zoomY.interpolator = OvershootInterpolator()
+                zoomY.duration = 500L
+                zoomY.doOnEnd { screen.remove() }
+
+                zoomX.start()
+                zoomY.start()
+            }
+        }
         enableEdgeToEdge()
         setContent {
             TuneFlowTheme {
